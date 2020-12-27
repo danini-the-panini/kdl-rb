2b0871cf80e95b101f98462f5cf866882f36287d
Daniel Smith <jellymann@gmail.com>
2020-12-27 11:31:18 +0200

    add: more tests

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,11 +1,181 @@
 require "test_helper"
 
 class ParserTest < Minitest::Test
-  # def setup
-  #   @parser = ::KDL::Parser.new
-  # end
+  def setup
+    @parser = ::KDL::Parser.new
+  end
 
-  # def test_nodes
-  #   assert_equal 
-  # end
+  def test_basic
+    doc = @parser.parse('title "Hello, World"')
+    nodes = nodes! {
+      title "Hello, World"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_multiple_values
+    doc = @parser.parse('bookmarks 12 15 188 1234')
+    nodes = nodes! {
+      bookmarks 12, 15, 188, 1234
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_properties
+    doc = @parser.parse <<~KDL
+      author "Alex Monad" email="alex@example.com" active=true
+      foo bar=true "baz" quux=false 1 2 3
+    KDL
+    nodes = nodes! {
+      author "Alex Monad", email: "alex@example.com", active: true
+      foo "baz", 1, 2, 3, bar: true, quux: false
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_nested_child_nodes
+    doc = @parser.parse <<~KDL
+      contents {
+        section "First section" {
+          paragraph "This is the first paragraph"
+          paragraph "This is the second paragraph"
+        }
+      }
+    KDL
+    nodes = nodes! {
+      contents {
+        section("First section") {
+          paragraph "This is the first paragraph"
+          paragraph "This is the second paragraph"
+        }
+      }
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_semicolon
+    doc = @parser.parse('node1; node2; node3;')
+    nodes = nodes! {
+      node1; node2; node3;
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_raw_strings
+    doc = @parser.parse <<~KDL
+      node "this\nhas\tescapes"
+      other r"C:\Users\zkat\"
+      other-raw r#"hello"world"#
+    KDL
+    nodes = nodes! {
+      node "this\nhas\tescapes"
+      other "C:\\Users\\zkat\\"
+      _ 'other-raw', "hello\"world"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_multiline_strings
+    doc = @parser.parse <<~KDL
+      string "my
+      multiline
+      value"
+    KDL
+    nodes = nodes! {
+      string "my\nmultiline\nvalue"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_numbers
+    doc = @parser.parse <<~KDL
+      num 1.234e-42
+      my-hex 0xdeadbeef
+      my-octal 0o755
+      my-binary 0b10101101
+      bignum 1_000_000
+    KDL
+    nodes = nodes! {
+      num 1.234e-42
+      _ 'my-hex', 0xdeadbeef
+      _ 'my-octal', 0o755
+      _ 'my-binary', 0b10101101
+      bignum 1_000_000
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_comments
+    doc = @parser.parse <<~KDL
+      // C style
+
+      /*
+      C style multiline
+      */
+
+      tag /*foo=true*/ bar=false
+
+      /*/*
+      hello
+      */*/
+    KDL
+    nodes = nodes! {
+      tag bar: false
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_slash_dash
+    doc = @parser.parse <<~KDL
+      /-mynode "foo" key=1 {
+        a
+        b
+        c
+      }
+
+      mynode /-"commented" "not commented" /-key="value" /-{
+        a
+        b
+      }
+    KDL
+    nodes = nodes! {
+      mynode "not commented"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_multiline_nodes
+    doc = @parser.parse <<~KDL
+      title \
+        "Some title"
+    KDL
+    nodes = nodes! {
+      title "Some title"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_utf8
+    doc = @parser.parse <<~KDL
+      smile "😁"
+      ノード お名前＝"☜(ﾟヮﾟ☜)"
+    KDL
+    nodes = ::KDL::Document.new([
+      ::KDL::Node.new('smile', [::KDL::Value::String.new('😁')]),
+      ::KDL::Node.new('ノード', [], { 'お名前' => ::KDL::Value::String.new('☜(ﾟヮﾟ☜)') })
+    ])
+    assert_equal nodes.to_s, doc.to_s
+  end
+
+  def test_node_names
+    doc = @parser.parse <<~KDL
+      "!@#$@$%Q#$%~@!40" "1.2.3" "!!!!!"=true
+      foo123~!@#$%^&*.:'|/?+ "weeee"
+    KDL
+    nodes = nodes! {
+      _ "!@#$@$%Q#$%~@!40" "1.2.3", "!!!!!": true
+      _ "foo123~!@#$%^&*.:'|/?+", "weeee"
+    }
+    assert_equal nodes.to_s, doc.to_s
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,41 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "kdl"
 
 require "minitest/autorun"
+
+class Minitest::Test
+
+  private
+
+  # Helper for constructing nodes
+  # Basically a Ruby DSL that looks a bit like KDL
+  def nodes!(&block)
+    ::KDL::Document.new(Nodes.nodes!(&block))
+  end
+
+  class Nodes < BasicObject
+    attr_reader :children
+
+    def initialize
+      @children = []
+    end
+
+    def method_missing(name, *args, **kwargs, &block)
+      node = ::KDL::Node.new(name,
+                             args.map { |a| ::KDL::Value.from(a) },
+                             kwargs.transform_keys(&:to_s)
+                                   .transform_values { |a| ::KDL::Value.from(a) })
+      node.children = Nodes.nodes!(&block) if block
+      @children << node
+    end
+    alias _ method_missing
+
+    def self.nodes!(&block)
+      n = new
+      n.instance_exec(&block)
+      n.children
+    end
+
+    define_method(:instance_exec, ::Object.instance_method(:instance_exec))
+    define_method(:block_given?, ::Object.instance_method(:block_given?))
+  end
+end
