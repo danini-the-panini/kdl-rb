b85be9aa5d4ceac60a21461ab748bf6fbe3bcd81
Danielle Smith <danini@hey.com>
2021-09-28 21:25:45 +0200

    change: formatting of spec test names

--- test/spec_test.rb
+++ test/spec_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class SpecTest < Minitest::Test
   TEST_CASES_DIR = File.join(__dir__, 'kdl-org/tests/test_cases')
@@ -9,13 +9,13 @@ class SpecTest < Minitest::Test
     input_name = File.basename(input_path, '.kdl')
     expected_path = File.join(EXPECTED_DIR, "#{input_name}.kdl")
     if File.exist?(expected_path)
-      define_method "test_input_#{input_name}_matches_expected_output" do
+      define_method "test_#{input_name}_matches_expected_output" do
         input = File.read(input_path)
         expected = File.read(expected_path)
         assert_equal expected, ::KDL.parse_document(input).to_s
       end
     else
-      define_method "test_input_#{input_name}_does_not_parse" do
+      define_method "test_#{input_name}_does_not_parse" do
         input = File.read(input_path)
         assert_raises { ::KDL.parse_document(input) }
       end
