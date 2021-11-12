7aa7ee19e35b1a7eab3820731ad9b173e8d04515
Danielle Smith <danini@hey.com>
2021-11-13 00:02:38 +0200

    add: better hostname parsing

--- test/types/hostname_test.rb
+++ test/types/hostname_test.rb
@@ -4,12 +4,21 @@ class HostnameTest < Minitest::Test
   def test_hostname
     value = KDL::Types::Hostname.call(::KDL::Value::String.new('www.example.com'))
     assert_equal 'www.example.com', value.value
+    refute_nil KDL::Types::Hostname.call(::KDL::Value::String.new('a'*63 + '.com'))
+    refute_nil KDL::Types::Hostname.call(::KDL::Value::String.new([63, 63, 63, 61].map { |x| 'a' * x }.join('.')))
 
     assert_raises { KDL::Types::Hostname.call(::KDL::Value::String.new('not a hostname')) }
+    assert_raises { KDL::Types::Hostname.call(::KDL::Value::String.new('-starts-with-a-dash.com')) }
+    assert_raises { KDL::Types::Hostname.call(::KDL::Value::String.new('a'*64 + '.com')) }
+    assert_raises { KDL::Types::Hostname.call(::KDL::Value::String.new((['a' * 63] * 4).join('.'))) }
   end
 
   def test_idn_hostname
     value = KDL::Types::IDNHostname.call(::KDL::Value::String.new('xn--bcher-kva.example'))
+    assert_equal 'bücher.example', value.value
+    assert_equal 'xn--bcher-kva.example', value.ascii_value
+
+    value = KDL::Types::IDNHostname.call(::KDL::Value::String.new('bücher.example'))
     assert_equal 'bücher.example', value.value
     assert_equal 'xn--bcher-kva.example', value.ascii_value
 
