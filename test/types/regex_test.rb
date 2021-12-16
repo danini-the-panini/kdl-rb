63d7780ecc20e359d66f1a402cb3d787154f45ea
Danielle Smith <danini@hey.com>
2021-12-16 23:38:22 +0200

    add: negative test for regex type

--- test/types/regex_test.rb
+++ test/types/regex_test.rb
@@ -4,5 +4,7 @@ class RegexTest < Minitest::Test
   def test_regex
     value = KDL::Types::Regex.call(::KDL::Value::String.new('asdf'))
     assert_equal(/asdf/, value.value)
+
+    assert_raises { KDL::Types::Regex.call(::KDL::Value::String.new('invalid(regex]')) }
   end
 end
