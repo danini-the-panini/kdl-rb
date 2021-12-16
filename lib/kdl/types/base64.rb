256f47c44f077307a9723971ed166ec0be64d568
Danielle Smith <danini@hey.com>
2021-12-16 23:33:33 +0200

    add: check for valid base64

--- lib/kdl/types/base64.rb
+++ lib/kdl/types/base64.rb
@@ -3,8 +3,14 @@ require 'base64'
 module KDL
   module Types
     class Base64 < Value
+      RGX = /^[A-Za-z0-9+\/=]+$/.freeze
+
       def self.call(value, type = 'base64')
         return nil unless value.is_a? ::KDL::Value::String
+
+        unless RGX.match?(value.value)
+            raise ArgumentError, "invalid base64: #{value.value}"
+      end
 
         data = ::Base64.decode64(value.value)
         new(data, type: type)
