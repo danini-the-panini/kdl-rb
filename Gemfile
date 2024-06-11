8fb84554b5b3a2db72921105564f83a2197b83ce
Dani Smith <code@danini.dev>
2024-06-11 10:25:22 +0200

    Add coverage (#18)
    * add coverage
    
    * add coveralls
    
    * add coveralls action to workflow
    
    * add badges
    
    * fix newline

--- Gemfile
+++ Gemfile
@@ -4,4 +4,9 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 12.0"
-gem "minitest", "~> 5.0"
+
+group :test do
+  gem "minitest", "~> 5.0"
+  gem "simplecov", require: false
+  gem "coveralls_reborn", require: false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require "simplecov"
+require "coveralls"
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "kdl"
 
